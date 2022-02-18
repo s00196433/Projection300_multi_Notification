9c2821f7c37b5f511745405e13889ff04f3d9dc1
s00196433 <s00196433@mail.itsligo.ie>
2022-02-18 01:05:06 +0000

    1 methods

--- app/src/main/java/zoe/itsligo/project300_notfi_mutlichannel4/MainActivity.java
+++ app/src/main/java/zoe/itsligo/project300_notfi_mutlichannel4/MainActivity.java
@@ -14,18 +14,28 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String NOTIFICATION_CHANNEL_ID = "10001";
+    // public static final String NOTIFICATION_CHANNEL_ID = "10001";
+
+  //  public static final String NOTIFICATION_CHANNEL_ID = null;
+
+    public String ChannelID;
+    public String ChannelDesc;
+
+    public static final int base_CHANNEL_ID = 10001;
+
     private final static String default_notification_channel_id = "default";
 
     public static final String NOTIFICATION2_CHANNEL_ID = "10002";
-    
+
+    //Button btnCreateNotification = findViewById(R.id.btnCreateNotification);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnCreateNotification = findViewById(R.id.btnCreateNotification);
-        btnCreateNotification.setOnClickListener(new View.OnClickListener() {
+    }
+       /* btnCreateNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //  Uri sound = Uri. parse (ContentResolver. SCHEME_ANDROID_RESOURCE + "://" + getPackageName() + "/raw/quite_impressed.mp3" ) ;
@@ -57,14 +67,28 @@ public class MainActivity extends AppCompatActivity {
                         mBuilder.build());
             }
         });
-    }
+    }*/
+       public void notification1(View view) {
+           ChannelID = Integer.toString(base_CHANNEL_ID + 1);
+           ChannelDesc="notification 1";
+           myMethod(ChannelID,ChannelDesc);
+       }
 
-    public void notification2(View view) {
+      public void notification2(View view) {
+          ChannelID = Integer.toString(base_CHANNEL_ID + 2);
+          ChannelDesc="notification 2";
+        myMethod(ChannelID,ChannelDesc);
+
+      }
+
+
+
+    void myMethod(String channel, String Description) {
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(MainActivity.this, default_notification_channel_id)
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentTitle("Test")
                 // .setSound(sound)
-                .setContentText("Second notification");
+                .setContentText(Description);
         NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             AudioAttributes audioAttributes = new AudioAttributes.Builder()
@@ -73,13 +97,13 @@ public class MainActivity extends AppCompatActivity {
                     .build();
             int importance = NotificationManager.IMPORTANCE_HIGH;
             NotificationChannel notificationChannel = new
-                    NotificationChannel(NOTIFICATION2_CHANNEL_ID, "NOTIFICATION_CHANNEL_NAME", importance);
+               /* NOTIFICATION_CHANNEL:*/    NotificationChannel(channel, "NOTIFICATION_CHANNEL_NAME", importance);
             notificationChannel.enableLights(true);
             notificationChannel.setLightColor(Color.RED);
             notificationChannel.enableVibration(true);
             notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
             //  notificationChannel.setSound(sound , audioAttributes) ;
-            mBuilder.setChannelId(NOTIFICATION2_CHANNEL_ID);
+            /* NOTIFICATION_CHANNEL:*/ mBuilder.setChannelId(channel);
             assert mNotificationManager != null;
             mNotificationManager.createNotificationChannel(notificationChannel);
         }
@@ -87,4 +111,6 @@ public class MainActivity extends AppCompatActivity {
         mNotificationManager.notify((int) System.currentTimeMillis(),
                 mBuilder.build());
     }
+
+
 }
